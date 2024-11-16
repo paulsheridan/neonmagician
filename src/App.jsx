0e8b23ad9e9a72f41942ef2a6f528ef851fe7bba
Paul <paul.sheridan@me.com>
2024-11-15 16:38:11 -0800

    fix broken images

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import { Flex } from "@chakra-ui/react";
 import Navbar from "./components/Landing/Navbar";
 import Footer from "./components/Landing/Footer";
 import Hero from "./components/Landing/Hero";
-import Logo from "./assets/images/grackleblue.png";
-import Pricing from "./components/Landing/Pricing";
-import Landing from "./components/Landing/Landing";
-import Current from "./components/Landing/Current";
 import ImageCarousel from "./components/Landing/ImageCarousel";
 import InstagramFeed from "./components/Landing/InstagramFeed";
 import FindUsSection from "./components/Landing/FindUsSection";
@@ -15,13 +11,7 @@ function App() {
   return (
     <Flex direction="column" w="100%">
       <Navbar />
-      <Hero
-        title="Welcome to Tattoo"
-        subtitle="It's called Grackle and it's for booking tattoos!"
-        image={Logo}
-        ctaText="Sign Up Now!"
-        ctaLink="/signup"
-      />
+      <Hero />
       <ImageCarousel />
       <InstagramFeed />
       <FindUsSection />
